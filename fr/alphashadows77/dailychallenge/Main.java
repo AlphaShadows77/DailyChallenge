efc66eeb7764abe03817e6b3ae27ac555c1bf0c8
AlphaShadows77 <alpha-shadows@hotmail.com>
2018-10-23 16:11:31 +0200

    Ajout d'un fichier config + loader

--- fr/alphashadows77/dailychallenge/Main.java
+++ fr/alphashadows77/dailychallenge/Main.java
@@ -1,12 +1,29 @@
 package fr.alphashadows77.dailychallenge;
 
+import java.io.File;
+
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	
+	private static FileConfiguration mainConfig;
+	
 	@Override
 	public void onEnable(){
 		
+		//Config Loading
+		loadConfig();
+		
+	}
+	
+	private void loadConfig(){
+		if (!new File(getDataFolder(), "config.yml").exists()){
+			saveDefaultConfig();
+			saveConfig();
+		}
+		
+		mainConfig = getConfig();
 	}
 	
 }
