bcb2a98f822e4bae6d0134ec67848f2b82897b75
AlphaShadows77 <alpha-shadows@hotmail.com>
2018-10-23 15:56:32 +0200

    Base du plugin

---
+++ fr/alphashadows77/dailychallenge/Main.java
@@ -0,0 +1,12 @@
+package fr.alphashadows77.dailychallenge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	@Override
+	public void onEnable(){
+		
+	}
+	
+}
