7e2b948c683a726d29414c4c187e35fe1cabf362
AlphaShadows77 <alpha-shadows@hotmail.com>
2019-03-05 23:51:14 +0100

    Ajout d'une fonction pour sauvegarder en utf-8

--- fr/alphashadows77/dailychallenge/Utils.java
+++ fr/alphashadows77/dailychallenge/Utils.java
@@ -1,8 +1,12 @@
 package fr.alphashadows77.dailychallenge;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -232,6 +236,49 @@ public class Utils {
 			main.getCustomConfig(pKey).save(new File(main.getDataFolder(), pKey + ".yml"));
 		}
 		catch (IOException e) {e.printStackTrace();}
+	}
+	
+	/**
+	 * Sauvegarde la config custom en utilisant l'encodage donné
+	 * @param key Key associated to a config
+	 * @param charset Charset to use
+	 */
+	public static void saveCustomConfig(String key, Charset charset) {
+		
+		FileConfiguration config = main.getCustomConfig(key);
+		String configToString = config.saveToString();
+		
+		File configFile = new File(main.getDataFolder(), key + ".yml");
+		OutputStreamWriter osw = null;
+		try {
+			
+			FileOutputStream fis = new FileOutputStream(configFile);
+			osw = new OutputStreamWriter(fis, charset);
+			
+			osw.write(configToString);
+			
+		}
+		
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		finally {
+			
+			try {
+				osw.close();
+			}
+			
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		
 	}
 	
 	
